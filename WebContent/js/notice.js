be5c31a2a9d13a5fe74606efb9b793256a8eb47a
cowboy9408 <cowboy9408@naver.com>
2022-08-04 13:56:21 +0900

    2022.08.04 공지사항 페이지 추가

---
+++ WebContent/js/notice.js
@@ -0,0 +1,36 @@
+function GetShow(){
+  var con = document.getElementById("plus");
+  if(con.style.display == "none"){
+      con.style.display = "block";
+  }
+  else{
+      con.style.display = "none";
+  }
+}
+function GetShow2(){
+  var con = document.getElementById("plus_2");
+  if(con.style.display == "none"){
+      con.style.display = "block";
+  }
+  else{
+      con.style.display = "none";
+  }
+}
+function GetShow3(){
+  var con = document.getElementById("plus_3");
+  if(con.style.display == "none"){
+      con.style.display = "block";
+  }
+  else{
+      con.style.display = "none";
+  }
+}
+function GetShow4(){
+  var con = document.getElementById("plus_4");
+  if(con.style.display == "none"){
+      con.style.display = "block";
+  }
+  else{
+      con.style.display = "none";
+  }
+}
