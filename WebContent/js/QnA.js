dd4c469c543925d3c0cd56554b629828d07b587f
cowboy9408 <cowboy9408@naver.com>
2022-08-08 15:52:29 +0900

    2022 .08.08 QnA 페이지 업로드

---
+++ WebContent/js/QnA.js
@@ -0,0 +1,52 @@
+function GetShow(){
+    var con = document.getElementById("ans_1");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    }
+    else{
+        con.style.display = "none";
+    }
+  }
+  function GetShow2(){
+    var con = document.getElementById("ans_2");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    }
+    else{
+        con.style.display = "none";
+    }
+  }
+  function GetShow3(){
+    var con = document.getElementById("ans_3");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    }
+    else{
+        con.style.display = "none";
+    }
+  }
+  function GetShow4(){
+    var con = document.getElementById("ans_4");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    }
+    else{
+        con.style.display = "none";
+    }
+  }function GetShow5(){
+    var con = document.getElementById("ans_5");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    }
+    else{
+        con.style.display = "none";
+    }
+  }function GetShow6(){
+    var con = document.getElementById("ans_6");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    }
+    else{
+        con.style.display = "none";
+    }
+  }
