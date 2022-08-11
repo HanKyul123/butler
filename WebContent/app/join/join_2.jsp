81ac26a7823de7ed63f76e59e651291a1ebd7199
G0RaNii <crosstu@nate.com>
2022-08-11 16:31:33 +0900

    2022 / 08 / 11 back 16:31

--- WebContent/app/join/join_2.jsp
+++ WebContent/app/join/join_2.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+   pageEncoding="UTF-8"%>
 
 
 
@@ -11,44 +11,44 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>휴대폰 본인 확인</title>
 <link rel="icon"
-	href="${pageContext.request.contextPath}/img/Hotel_icon.png">
+   href="${pageContext.request.contextPath}/img/Hotel_icon.png">
 <link rel="stylesheet"
-	href="${pageContext.request.contextPath}/css/join2.css">
+   href="${pageContext.request.contextPath}/css/join2.css">
 </head>
 <body>
-	<div class="wrap">
-		<div class="phone_title">
-			<strong>휴대폰 본인 확인</strong>
-		</div>
-		<p>원활한 서비스 제공을 위해, 휴대폰 번호를 입력해주세요.</p>
-		<form name="phone_prove" method="post" action="${cp}/login/loginPhoneProve.us">
-			<div class="Pnum_box">
-				<div id="sendCode">
-					<strong class="bold">휴대폰 번호</strong>
-					<section>
-						<div class="Wtext">
-							<input id="phone_num" name="user_phone" type="tel" minlength="11" maxlength="13">
-							<button type="button" class=" btn_checked1" id="click_btn" onclick="sendcode()">인증번호 전송</button>
-							<input type="hidden" id="phone_num_1" name="user_phone_1" value="">
-						</div>
-					</section>
-				</div>
-				<div class="Prove_box">
-					<strong class="bold2">인증번호</strong>
-					<div class="Wnum">
-						<input id="prove_num" type="tel" minlength="4" maxlength="4" autocomplete="off">
-						<button type="submit" class="btn_checked2" id="click_btn" disabled="disabled">확인</button>
-						<div class="time">
-							<span id="timer" class="timer">3:00</span>
-						</div>
-						<div class="warytxtbox">
-							<span class="warytxt"></span>
-						</div>
-					</div>
-				</div>
-			</div>
-		</form>
-	</div>
+   <div class="wrap">
+      <div class="phone_title">
+         <strong>휴대폰 본인 확인</strong>
+      </div>
+      <p>원활한 서비스 제공을 위해, 휴대폰 번호를 입력해주세요.</p>
+      <form name="phone_prove" method="post" action="${cp}/login/loginPhoneProve.us">
+         <div class="Pnum_box">
+            <div id="sendCode">
+               <strong class="bold">휴대폰 번호</strong>
+               <section>
+                  <div class="Wtext">
+                     <input id="phone_num" name="user_phone" type="tel" minlength="11" maxlength="13">
+                     <button type="button" class=" btn_checked1" id="click_btn" onclick="sendcode()">인증번호 전송</button>
+                     <input type="hidden" id="phone_num_1" name="user_phone_1" value="">
+                  </div>
+               </section>
+            </div>
+            <div class="Prove_box">
+               <strong class="bold2">인증번호</strong>
+               <div class="Wnum">
+                  <input id="prove_num" type="tel" minlength="4" maxlength="4" autocomplete="off">
+                  <button type="submit" class="btn_checked2" id="click_btn" disabled="disabled">확인</button>
+                  <div class="time">
+                     <span id="timer" class="timer">3:00</span>
+                  </div>
+                  <div class="warytxtbox">
+                     <span class="warytxt"></span>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </form>
+   </div>
 
 </body>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
--- src/com/butler/app/action/KakaoLoginAction.java
+++ src/com/butler/app/action/KakaoLoginAction.java
@@ -10,67 +10,84 @@ import com.butler.app.action.Action;
 import com.butler.app.action.ActionTo;
 import com.butler.app.dao.KUserDAO;
 import com.butler.app.dao.KUserDTO;
+import com.butler.app.dao.UserDTO;
 
 
 public class KakaoLoginAction implements Action {
 
-	@Override
-	public ActionTo execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-		resp.setCharacterEncoding("utf-8");
-		resp.setContentType("text/html; charset=utf-8");
-				
-		System.out.println("1");
+   @Override
+   public ActionTo execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+      resp.setCharacterEncoding("utf-8");
+      resp.setContentType("text/html; charset=utf-8");
+            
+      System.out.println("1");
 
-		KUserDTO user = new KUserDTO();
-		KUserDAO kdao = new KUserDAO();
-		
-		
-		String user_name = req.getParameter("user_name");
-//		String user_name = req.getParameter("user_name");
-		String user_email = req.getParameter("user_email");
-		
-		System.out.println(user_name);
-		System.out.println(user_email);
-		
-		if(kdao.kakaoDBcheck(user_name, user_email) != null) {
-			//기존 가입자
-			
-			System.out.println("2");
-			
-			String logintype = String.valueOf(kdao.kakaoDBcheck(user_name, user_email).getUser_logintype());
-			System.out.println(logintype);
-			
-			req.setAttribute("user_logintype",kdao.kakaoDBcheck(user_name, user_email).getUser_logintype());
-			req.setAttribute("user_num",kdao.kakaoDBcheck(user_name, user_email).getUser_num());
-			req.setAttribute("user_status",kdao.kakaoDBcheck(user_name, user_email).getUser_status());
-			req.setAttribute("user_identity",kdao.kakaoDBcheck(user_name, user_email).getUser_identity());
-			req.setAttribute("nickname",kdao.kakaoDBcheck(user_name, user_email).getNickname());
-			req.setAttribute("user_pw",kdao.kakaoDBcheck(user_name, user_email).getUser_pw());
-			req.setAttribute("user_name",kdao.kakaoDBcheck(user_name, user_email).getUser_name());
-			req.setAttribute("user_addr",kdao.kakaoDBcheck(user_name, user_email).getUser_addr());
-			req.setAttribute("user_email",kdao.kakaoDBcheck(user_name, user_email).getUser_email());
-			req.setAttribute("user_phone",kdao.kakaoDBcheck(user_name, user_email).getUser_phone());
-	
-//			req.setAttribute("user_logintype", user_logintype);
-			ActionTo transfer = new ActionTo();
-			transfer.setRedirect(true);
-			transfer.setPath("/testview.jsp");
-			return transfer;
-				
-		}
-		else {
-			//신규 가입자
-			System.out.println("3");
-			
-			HttpSession session = req.getSession(true);
-			session.setAttribute("user_name", user_name);
-			session.setAttribute("user_email", user_email);
-			
-			ActionTo transfer = new ActionTo();
-			transfer.setRedirect(true);
-			transfer.setPath("/app/join/join_1.jsp");
-			return transfer;
-		}
-	}
+      UserDTO user = new UserDTO();
+      KUserDAO kdao = new KUserDAO();
+
+      String user_name = req.getParameter("user_name");
+      String user_email = req.getParameter("user_email");
+      int user_logintype = 1;
+      
+      user.setUser_name(user_name);
+      user.setUser_email(user_email);
+      user.setUser_logintype(user_logintype);
+      
+      HttpSession session = req.getSession(true);
+      session.setAttribute("LoginUser", user);
+      
+      UserDTO useruser = (UserDTO)session.getAttribute("LoginUser");
+      System.out.println(useruser.getUser_name());
+      
+      
+      ActionTo transfer = new ActionTo();
+      transfer.setRedirect(true);
+      transfer.setPath("/app/join/join_1.jsp");
+      return transfer;
+      
+      
+//      System.out.println(user_name);
+//      System.out.println(user_email);
+//      
+//      if(kdao.kakaoDBcheck(user_name, user_email) != null) {
+//         //기존 가입자
+//         
+//         System.out.println("2");
+//         
+//         String logintype = String.valueOf(kdao.kakaoDBcheck(user_name, user_email).getUser_logintype());
+//         System.out.println(logintype);
+//         
+//         req.setAttribute("user_logintype",kdao.kakaoDBcheck(user_name, user_email).getUser_logintype());
+//         req.setAttribute("user_num",kdao.kakaoDBcheck(user_name, user_email).getUser_num());
+//         req.setAttribute("user_status",kdao.kakaoDBcheck(user_name, user_email).getUser_status());
+//         req.setAttribute("user_identity",kdao.kakaoDBcheck(user_name, user_email).getUser_identity());
+//         req.setAttribute("nickname",kdao.kakaoDBcheck(user_name, user_email).getNickname());
+//         req.setAttribute("user_pw",kdao.kakaoDBcheck(user_name, user_email).getUser_pw());
+//         req.setAttribute("user_name",kdao.kakaoDBcheck(user_name, user_email).getUser_name());
+//         req.setAttribute("user_addr",kdao.kakaoDBcheck(user_name, user_email).getUser_addr());
+//         req.setAttribute("user_email",kdao.kakaoDBcheck(user_name, user_email).getUser_email());
+//         req.setAttribute("user_phone",kdao.kakaoDBcheck(user_name, user_email).getUser_phone());
+//   
+////         req.setAttribute("user_logintype", user_logintype);
+//         ActionTo transfer = new ActionTo();
+//         transfer.setRedirect(true);
+//         transfer.setPath("/testview.jsp");
+//         return transfer;
+//            
+//      }
+//      else {
+//         //신규 가입자
+//         System.out.println("3");
+//         
+//         HttpSession session = req.getSession(true);
+//         session.setAttribute("user_name", user_name);
+//         session.setAttribute("user_email", user_email);
+//         
+//         ActionTo transfer = new ActionTo();
+//         transfer.setRedirect(true);
+//         transfer.setPath("/app/join/join_1.jsp");
+//         return transfer;
+//      }
+   }
 
 }
