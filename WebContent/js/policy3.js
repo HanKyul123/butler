8c89b787f53df10d8599f8b3a1d04ba969da675b
cowboy9408 <cowboy9408@naver.com>
2022-08-12 17:37:03 +0900

    0222022.08.12 더보기단 모든 페이지 업로드 완료 (css, js 적용).08.12 더보기란 페이지 모두 업로드 (

---
+++ WebContent/js/policy3.js
@@ -0,0 +1,90 @@
+function GetShow(){
+    var con = document.getElementById("list_text");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    }
+    else{
+        con.style.display = "none";
+    }
+  }
+  function GetShow2(){
+    var con = document.getElementById("list_text_2");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    }
+    else{
+        con.style.display = "none";
+    }
+  }
+  function GetShow3(){
+    var con = document.getElementById("list_text_3");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    }
+    else{
+        con.style.display = "none";
+    }
+  }
+  function GetShow4(){
+    var con = document.getElementById("list_text_4");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    }
+    else{
+        con.style.display = "none";
+    }
+  }
+  function GetShow5(){
+    var con = document.getElementById("list_text_5");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    }
+    else{
+        con.style.display = "none";
+    }
+  }
+  function GetShow6(){
+    var con = document.getElementById("list_text_6");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    }
+    else{
+        con.style.display = "none";
+    }
+  }
+  function GetShow7(){
+    var con = document.getElementById("list_text_7");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    }
+    else{
+        con.style.display = "none";
+    }
+  }
+  function GetShow8(){
+    var con = document.getElementById("list_text_8");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    }
+    else{
+        con.style.display = "none";
+    }
+  }
+  function GetShow9(){
+    var con = document.getElementById("list_text_9");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    }
+    else{
+        con.style.display = "none";
+    }
+  }
+  function GetShow10(){
+    var con = document.getElementById("list_text_10");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    }
+    else{
+        con.style.display = "none";
+    }
+  }
