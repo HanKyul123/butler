75e0b2e419583b89f9c416d9c25ef467657ff76c
KangCuty <goldbaby1@naver.com>
2022-08-24 15:09:02 +0900

    myinfo js, css 추가 구현

--- WebContent/js/myinfo.js
+++ WebContent/js/myinfo.js
@@ -26,7 +26,7 @@ function checknick(value){
       txtnick.innerHTML="";
       txtnick.style.color="red";
       $('.write_new_nick').css("border","1px solid rgb(166, 166, 166)")
-      $('.complete_nick_btn').attr("disabled",true);
+      $('.modify_nick_btn').attr("disabled",true);
       return false;
     }
 
@@ -34,23 +34,20 @@ function checknick(value){
       txtnick.innerHTML="2~10자의 한글, 영문, 숫자만 사용할 수 있습니다.";
       txtnick.style.color="red";
       $('.write_new_nick').css("border","1px solid red")
-      $('.complete_nick_btn').attr("disabled",true);
-      return false;
+      $('.modify_nick_btn').attr("disabled",true);
 
       }else if(value==""){
-          txtnick.innerHTML="";
-          txtnick.style.color="red";
-          $('.write_new_nick').css("border","1px solid rgb(166, 166, 166)")
-          $('.complete_nick_btn').attr("disabled",true);
-          return false;
-        }
+      txtnick.innerHTML="";
+      txtnick.style.color="red";
+      $('.write_new_nick').css("border","1px solid rgb(166, 166, 166)")
+      $('.modify_nick_btn').attr("disabled",true);
+  }
   else{
       txtnick.innerHTML="사용 가능한 닉네임 입니다.";
       txtnick.style.color="rgb(19, 106, 227)";
       $('.write_new_nick').css("border","1px solid rgb(166, 166, 166)")
-      $('.complete_nick_btn').attr("disabled",false);
+      $('.modify_nick_btn').attr("disabled",false);
   }
-  return true;
 }
 
 
@@ -61,6 +58,11 @@ function myFunction() {
     var z = document.getElementById("CMB");
     var d = document.getElementById("XB1");
     var w = document.getElementById("wary1");
+    var n = document.getElementById("checknick");
+
+    $(".modify_phone_btn").attr("disabled",true);
+    $(".modify_addr_btn").attr("disabled",true);
+    $(".modify_pw").attr("onclick","");
 
 
       x.style.display = "block";
@@ -68,6 +70,7 @@ function myFunction() {
       y.style.display = "none";
       z.style.display = "block";
       w.style.display = "block";
+      n.style.display = "block";
 
       x.focus();
   }
@@ -78,6 +81,13 @@ function myFunction_1() {
     var y = document.getElementById("MNB");
     var z = document.getElementById("CMB");
     var w = document.getElementById("wary1");
+    var m = document.getElementById("checknick");
+
+    $(".modify_phone_btn").attr("disabled",false);
+    $(".modify_addr_btn").attr("disabled",false);
+    $(".modify_pw").attr("onclick","change_pw()");
+
+
 
       x.style.display = "none";
       d.style.display = "none";
@@ -85,6 +95,8 @@ function myFunction_1() {
       y.style.display = "block";
       z.style.display = "none";
       w.style.display = "none";
+      m.style.display = "none";
+
 
       if(x.value!=""){
         user_nick.innerHTML=x.value;
@@ -131,6 +143,10 @@ function myFunction2() {
     var c = document.getElementById("SC");
     var d = document.getElementById("XB");
    
+
+    $(".modify_nick_btn").attr("disabled",true);
+    $(".modify_addr_btn").attr("disabled",true);
+    $(".modify_pw").attr("onclick","");
     
       x.style.display = "block";
       y.style.display="none"
@@ -141,28 +157,36 @@ function myFunction2() {
   }
 
   const Pvalue = document.getElementById("WNP");
-  function myFunction2_1() {
-	    var x = document.getElementById("WNP");
-	    var y = document.getElementById("MPB");
-	    var c = document.getElementById("SC");
-	    var d = document.getElementById("XB");
-	    
-	    
-	    Pvalue.value="";
-	    Pvalue.style.border="1px solid rgb(166, 166, 166)";
-	    x.style.display = "none";
-	    y.style.display="block";
-	    c.style.display = "none";
-	      d.style.display = "none";
-	      
-	    }
+function myFunction2_1() {
+    var x = document.getElementById("WNP");
+    var y = document.getElementById("MPB");
+    var c = document.getElementById("SC");
+    var d = document.getElementById("XB");
+
+    $(".modify_nick_btn").attr("disabled",false);
+    $(".modify_addr_btn").attr("disabled",false);
+    $(".modify_pw").attr("onclick","change_pw()");
+    
+    
+    Pvalue.value="";
+    Pvalue.style.border="1px solid rgb(166, 166, 166)";
+    x.style.display = "none";
+    y.style.display="block";
+    c.style.display = "none";
+      d.style.display = "none";
+      
+    }
 function myFunction2_2() {
     var x = document.getElementById("WNN");
     var d = document.getElementById("XB1");
     var y = document.getElementById("MNB");
     var z = document.getElementById("CMB");
     var w = document.getElementById("wary1");
+    var m = document.getElementById("checknick");
 
+    $(".modify_phone_btn").attr("disabled",false);
+    $(".modify_addr_btn").attr("disabled",false);
+    $(".modify_pw").attr("onclick","change_pw()");
     
     
     x.style.display = "none";
@@ -170,6 +194,7 @@ function myFunction2_2() {
     y.style.display = "block";
     z.style.display = "none";
     w.style.display = "none";
+    m.style.display = "none";
     $('#MNB').attr("disabled",false);
     x.value="";
     txtnick.innerHTML="";
@@ -215,7 +240,9 @@ function myFunction2_2() {
         x.style.display = "block";
         y.style.display="block";
         d.style.display = "none";
+        x.value="";
         x.focus();
+
 
 
         var time = 179; // 기준시간 3분 (180/60 =3)
@@ -241,7 +268,56 @@ var x = setInterval(function(){
     },1000)
         
     }
+    
+    
+    function myFunction3_2() {
+      var x = document.getElementById("WPC2");
+      var y = document.getElementById("PCB2");
+      var e = document.getElementById("time2");
+      
+      $('.send_code2').attr("disabled", true);
+        e.style.display="block";
+        x.style.display = "block";
+        y.style.display="block";
+        x.value="";
+        x.focus();
 
+
+
+        var time = 179; // 기준시간 3분 (180/60 =3)
+var min = ""; // 분
+var sec = ""; // 초
+
+var x = setInterval(function(){
+    min = parseInt(time/60); // 3분
+    sec = time%60; //나머지 초
+
+    document.getElementById("timer2").innerHTML = min +":"+sec; // 3:00
+    time--;
+
+    //타임아웃 시
+    if(time<0){
+        clearInterval(x); //setInterval() 실행을 끝내기
+        document.getElementById("timer2").innerHTML = "0:00"
+        $('.send_code2').attr("disabled", false);
+
+        
+
+        }
+        $(".modify_pw2").on("click",function(){
+
+          clearInterval(x); //setInterval() 실행을 끝내기
+          document.getElementById("timer2").innerHTML = "3:00"
+          $('.send_code2').attr("disabled", true);
+  
+        })
+    },1000)
+        
+    }
+    
+
+
+    
 // 주소 수정
     function myFunction4() {
         var y = document.getElementById("MAB");
@@ -253,7 +329,11 @@ var x = setInterval(function(){
         var g = document.getElementById("sample6_extraAddress");
         var x = document.getElementById("XB2");
 
-        $('.complete_addr_btn').attr("disabled", true);
+        $(".modify_nick_btn").attr("disabled",true);
+        $(".modify_phone_btn").attr("disabled",true);
+        $(".modify_pw").attr("onclick","");
+        
+
           c.style.display = "block";
           d.style.display = "block";
           e.style.display = "block";
@@ -277,7 +357,11 @@ var x = setInterval(function(){
         var f = document.getElementById("sample6_detailAddress");
         var g = document.getElementById("sample6_extraAddress");
 
-        if(e.value!=""&&f.value!=""){
+        $(".modify_nick_btn").attr("disabled",false);
+        $(".modify_phone_btn").attr("disabled",false);
+        $(".modify_pw").attr("onclick","change_pw()");
+
+        if(e.value!=""){
           
           txtaddrs.innerHTML=e.value+" "+f.value;
 
@@ -315,7 +399,9 @@ var x = setInterval(function(){
         var f = document.getElementById("sample6_detailAddress");
         var g = document.getElementById("sample6_extraAddress");
 
-
+        $(".modify_nick_btn").attr("disabled",false);
+        $(".modify_phone_btn").attr("disabled",false);
+        $(".modify_pw").attr("onclick","change_pw()");
         
           c.style.display = "none";
           d.style.display = "none";
@@ -334,6 +420,38 @@ var x = setInterval(function(){
        
       }
 
+      function finish_prove(){
+        var w = document.getElementById("WNP");
+        var x = document.getElementById("WPC");
+        var y = document.getElementById("PCB");
+        var d = document.getElementById("XB");
+        var e = document.getElementById("time");
+        var m = document.getElementById("MPB");
+        var s = document.getElementById("SC");
+        var u = document.getElementById("USP");
+
+        $(".modify_nick_btn").attr("disabled",false);
+        $(".modify_addr_btn").attr("disabled",false);
+        $(".modify_pw").attr("onclick","change_pw()");
+
+        let str = w.value;
+        let phonenum = str.slice(0,3)+"-"+str.slice(3,7)+"-"+str.slice(7,11);
+
+        x.style.display="none";
+        m.style.display="block";
+        y.style.display="none";
+        d.style.display="none";
+        e.style.display="none";
+        w.style.display="none";
+        s.style.display="none";
+        x.value="";
+
+        if(w.value!==""){
+          u.innerHTML=phonenum;
+        }
+        w.value="";
+
+      }
 
 
       
@@ -369,87 +487,87 @@ $('.write_phone_code').on('input',function(){
       }
   }
 })
+var wc2 = document.querySelector("#WPC2");
+function change_pw(){
+
+  let x = document.getElementById("WNP2");
+  let y = document.getElementById("SC2");
+  var z = document.getElementById("MDPW");
+  var z2 = document.getElementById("MDPW2");
+  let a = document.getElementById("ELT");
+
+        $(".modify_nick_btn").attr("disabled",true);
+        $(".modify_addr_btn").attr("disabled",true);
+        $(".modify_phone_btn").attr("onclick",true);
 
 
+  z.style.display="none";
+  z2.style.display="block";
+  x.style.display="block";
+  y.style.display="block";
+  a.style.display="block";
 
-  function addr_chk(addr){
-    // 한글,띄어쓰기 가능
-    var chk = /[0-9]|[가-힣]|[\s]/g;
-    // 자음, 모음만은 불가능   
-    var pattern = /([^가-힣\x20])/i;
-
-    if(addr.length==0){
-      $('.complete_addr_btn').attr("disabled", true);
-      return false;
-    }
-
-    if(!pattern.test(addr)&&chk.test(addr)){
-
-    }else{
-        return false;
-    }
-  return true;
-  }
-
-  function addr_chk2(addr){
-    if(addr.length==0){
-      $('.complete_addr_btn').attr("disabled", true);
-      return false;
-    }else{
-
-    }
-    return true;
-    
-  }
-
-  function checkaddr2(value){
-    if(!addr_chk2(value)){
-      $('.complete_addr_btn').attr("disabled", true);
-      return false;
-    }else{
-      $('.complete_addr_btn').attr("disabled", false);
-      
-    }
-
-  }
-
-
-
-
-  function checkaddr(value){
-    if(value==""){
-        txtaddr.innerHTML=""
-        txtaddr.style.color="red";
-        $('#sample6_detailAddress').css("border","1px solid rgb(166, 166, 166)");
-        $('.complete_addr_btn').attr("disabled", true);
-
-        return false;
-    }
-
-    if(!addr_chk(value)){
-        txtaddr.innerHTML="알맞은 형식으로 입력해주세요";
-        txtaddr.style.color="red";
-        $('#sample6_detailAddress').css("border","1px solid red")
-        $('.complete_addr_btn').attr("disabled", true);
-        return false;
-    }else if(value.length<1){
-        txtaddr.innerHTML="";
-        $('#sample6_detailAddress').css("border","1px solid rgb(166, 166, 166)");
-        $('.complete_addr_btn').attr("disabled", true);
-        return false;
-
-        
-    }else if($('#sample6_address').val()==""){
-      $('.complete_addr_btn').attr("disabled", true);
-      return false;
-
-    }else{
-        $('.complete_addr_btn').attr("disabled", false);
-    }
-        
-
-    
 }
 
+function cancel_pw(){
+  let x = document.getElementById("WNP2");
+  let y = document.getElementById("SC2");
+  var z = document.getElementById("MDPW");
+  var z2 = document.getElementById("MDPW2");
+  let a = document.getElementById("ELT");
+  let q = document.getElementById("PCB2");
+  let r = document.getElementById("WPC2");
+  var e = document.getElementById("time2");
+
+        $(".modify_nick_btn").attr("disabled",false);
+        $(".modify_addr_btn").attr("disabled",false);
+        $(".modify_phone_btn").attr("onclick",false);
+
+
+  z.style.display="block";
+  z2.style.display="none";
+  x.style.display="none";
+  y.style.display="none";
+  a.style.display="none";
+  r.style.display="none";
+  q.style.display="none";
+  e.style.display="none";
+
+  $(".write_new_phone2").css("border","1px solid rgb(166, 166, 166)");
+  x.value="";
+  wc2.value="";
+}
+
+
+var cs2 = document.querySelector("#WNP2");
+
+$('.write_new_phone2').on('input',function(){
+  if($('.write_new_phone2').val()==''){
+      $('.send_code2').attr("disabled", true);
+      $('.write_new_phone2').css("border","1px solid rgb(166, 166, 166)")
+  }else{
+      if(cs2.value.length >= 10){
+      $('.send_code2').attr("disabled", false);
+      $('.write_new_phone2').css("border","1px solid rgb(166, 166, 166)")
+    }
+      else if(cs2.value.length<11){
+        $('.send_code2').attr("disabled", true);
+        $('.write_new_phone2').css("border","1px solid red")
+      }
+  }
+})
+
+
+$('.write_phone_code2').on('input',function(){
+  if($('.write_phone_code2').val()==''){
+      $('.phone_code_btn2').attr("disabled", true);
+  }else{
+      if(wc2.value.length == 4){
+      $('.phone_code_btn2').attr("disabled", false);}
+      else if(wc2.value.length<4){
+        $('.phone_code_btn2').attr("disabled", true);
+      }
+  }
+})
 
 
