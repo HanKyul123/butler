337dd214d444f3df2e8354e0e7c1b589eb78d3bf
G0RaNii <crosstu@nate.com>
2022-08-28 15:38:39 +0900

    2022 / 08 / 28 15:38 mySql db 수정 비상시 전에 쓰던거 백업본 만듬

--- src/sql/butler_db.sql
+++ src/sql/butler_db.sql
@@ -4,19 +4,20 @@ drop database butler;
 use butler;
 
 create table user_tb(
-   user_logintype		int default 0,					#0= 일반가입자 / 1=카카오톡가입자
    USER_NUM_PK         int primary key auto_increment,
-   user_status         int default 1,               	#0=삭제된 회원, 1=사용중 회원
-   user_identity       int default 0,               	#0= 일반회원 / 1=사업자
+   user_logintype      int default 0,               #0= 일반가입자 / 1=카카오톡가입자
+   user_status         int default 1,                  #0=삭제된 회원, 1=사용중 회원
+   user_identity       int default 0,                  #0= 일반회원 / 1=사업자
    user_nickname       varchar(12) unique not null ,
    user_pw             varchar(30) not null,
    user_name           varchar(100) not null,
-   user_zipcode        varchar(12) not null,			#우편번호
+   user_zipcode        varchar(12) not null,         #우편번호
    user_addr           varchar(200) not null,
-   user_addrdetail     varchar(200),			#상세주소
-   user_addretc		   varchar(200),
+   user_addrdetail     varchar(200),         #상세주소
+   user_addretc         varchar(200),
    user_email          varchar(200) unique not null,
-   user_phone          varchar(15) not null
+   user_phone          varchar(15) not null,
+   user_business_num   varchar(30)      #사업자 번호
 );
 
 
@@ -25,22 +26,24 @@ drop table business_tb;
 
 
 create table business_tb(
-    BUSINESS_PLACE_NUM_PK    int primary key auto_increment not null,
-    business_name         varchar(200) not null,
-    business_zipcode     varchar(12) not null,   
-    business_addr         varchar(200) not null,
+    BUSINESS_PLACE_NUM_PK   int primary key auto_increment not null,
+    business_name           varchar(200) not null,
+    business_zipcode        varchar(12) not null,   
+    business_addr           varchar(200) not null,
     business_addrdetail     varchar(200),
-    business_addretc       varchar(200),
-    business_phone         varchar(15)   not null,
-    business_email         varchar(200),
-    business_category      int not null,
-    business_hashtag       varchar(200),
-    business_main_intro       varchar(200),         #호텔 메인 소개말
+    business_addretc        varchar(200),
+    business_phone          varchar(15)   unique not null,
+    business_email          varchar(200),
+    business_category       int not null,
+    business_hashtag        varchar(200),
+    business_main_intro     varchar(200),         #호텔 메인 소개말
     business_workplace_detail   text(10000),         #호텔 상세 소개글
     business_workplace_rule      text(10000),
     business_workplace_score   int   default 0,         #호텔 별점
+    business_pick_animal_type   int,            #대표동물
+    business_min_charge       int,               #최소금액
     USER_NUM_FK            int,
-    constraint bs_tb_USER_NUM_FK   foreign key (USER_NUM_FK) references  user_tb (USER_NUM_PK)
+	constraint bs_tb_USER_NUM_FK   foreign key (USER_NUM_FK) references  user_tb (USER_NUM_PK)
 );
 
 
@@ -72,12 +75,14 @@ create table pets_tb(
 
 
 create table review_tb(
-   REVIEW_NUM_PK            int primary key auto_increment,
+    REVIEW_NUM_PK             int primary key auto_increment,
     BUSINESS_PLACE_NUM_FK      int,
-   constraint r_tb_BUSINESS_PLACE_NUM_FK foreign key(BUSINESS_PLACE_NUM_FK) references business_tb (USER_NUM_FK),
+   constraint r_tb_BUSINESS_PLACE_NUM_FK foreign key(BUSINESS_PLACE_NUM_FK) references business_tb (BUSINESS_PLACE_NUM_PK),
     review_contents            varchar(5000),
     review_nickname            varchar(12),
-    review_regdate            datetime   default now()
+    review_regdate             datetime   default now(),
+    review_file_systemname      varchar(2000),
+    review_file_orgname         varchar(2000)
 );
 
 create table reply_tb(
@@ -117,4 +122,12 @@ create table basket_tb(
     constraint bk_tb_USER_NUM_FK   foreign key(USER_NUM_FK) references  user_tb (USER_NUM_PK),   
    BUSINESS_PLACE_NUM_FK      int,
     constraint bk_tb_BUSINESS_PLACE_NUM_FK foreign key(BUSINESS_PLACE_NUM_FK) references business_tb (BUSINESS_PLACE_NUM_PK)
+);
+
+create table business_charge_tb(
+   BUSINESS_PLACE_NUM_FK      int,
+   constraint bc_tb_BUSINESS_PLACE_NUM_FK foreign key(BUSINESS_PLACE_NUM_FK) references business_tb (BUSINESS_PLACE_NUM_PK),      
+    animal_type      int,               # 0. 강아지 1. 고양이 2. 새 3. 파충류 4. 패럿 6. 토끼 7. 햄스터 8. etc
+    animal_weight   varchar(200),         #동물무게 1 ~ 5kg / 6 ~ 10kg / 11 ~ 15kg / 16 ~ 20kg / 20kg이상
+    one_day_charge   int  #1박시 금액
 );
